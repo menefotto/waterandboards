e016b9f62b266f57e5827d8340c8bd5b85497b46
wind85 <locci.carlo.85@gmail.com>
2017-04-12 15:19:01 +0200

    remove local server due to use of gulp file

--- server.js
+++
@@ -1,6 +0,0 @@
-var app = require('express')
-var msg = function () { 
-    console.log('Your React App is live at http://localhost:3000') 
-}
-
-app().use('/', app.static('./public/')).listen(3000, msg)
