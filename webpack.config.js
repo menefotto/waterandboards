f3e5ac80ec6165628c13d0cc4c95f04eb7c072e1
wind85 <locci.carlo.85@gmail.com>
2017-04-16 22:01:56 +0200

    using preact to reduce size

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const webpack = require('webpack');
 // does it build?
 config = {
+  devtool: 'source-map',
   entry: './src/app.js',
   output: {
     filename: 'bin/bundle.js'
+  },
+  resolve: {
+    alias: {
+     'react': 'preact-compat',
+      'react-dom': 'preact-compat',
+      'react-redux': 'preact-redux',
+      'react-tap-event-plugin': 'preact-tap-event-plugin'
+    },
   },
   module: {
     loaders: [
@@ -12,11 +21,14 @@ config = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'stage-0', 'react','react-hmre']
+          presets: ['es2015', 'stage-0', 'react']
         }
       }
     ]
   },
+  plugins: [
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
   devServer: {
     contentBase: './src/',
     compress: true,
